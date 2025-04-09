aa90e1eb79b23dde1d846a98c72b9e815bd85f8f
David Thyresson <dthyresson@gmail.com>
2025-04-09 15:08:41 -0400

    clarify agents

--- src/app/agents/routes.ts
+++ src/app/agents/routes.ts
@@ -12,27 +12,14 @@ export const agentRoutes = [
   route("/*", async ({ request }: RequestInfo) => {
     // Automatically routes HTTP requests and/or WebSocket connections to /agents/:agent/:name
     // Best for: connecting React apps directly to Agents using useAgent from agents/react
+    // The MashupAgent is defined in the MashupAgent.ts file
+    // Wrangler durable_objects config defines the agent name as MASHUP_AGENT and the class name as MashupAgent
+    // So /agents/mashup-agent/default will route to the MashupAgent when an agent client
+    // uses the `useAgent` hook for websockets updates
 
     return (
       (await routeAgentRequest(request, env)) ||
       Response.json({ msg: "no agent here" }, { status: 404 })
     );
-
-    // const agent = await getAgentByName<Env, MyAgent>(
-    //   env.MY_AGENT,
-    //   "my-unique-agent-id",
-    // );
-    // if (!agent) {
-    //   return new Response("Agent not found", { status: 404 });
-    // }
-
-    // console.log("MyAgent agent", agent);
-
-    // const response = await agent.onRequest(request);
-
-    // return new Response(response.body, {
-    //   status: response.status,
-    //   headers: response.headers,
-    // });
   }),
 ];
